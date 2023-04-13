916ba11b2c1d22db10154c1172e517f9c8bf955e
arvindhsubramanian <90575131+arvindhsubramanian@users.noreply.github.com>
2023-04-14 01:28:14 +0530

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,15 +1,12 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Daneel</h1>
+    <h1 className="text-2xl font-bold mb-2">👨‍🏫 Negotiation Games - WAC Grades! </h1>
     <p>
-      You can use me to create your own chat bot app. In this demo I am an
-      expert in movie reviews, so feel free to ask me about movies! Then, create
-      your own bot in under five minutes.
+      Welcome to the wacky world of WAC negotiations!
+      Are you tired of getting mediocre grades in WAC? Are you convinced that your professor has a personal vendetta against you? Well, fear not my friend, because now you have the chance to negotiate your way to a higher grade!
+      Picture this: you're sitting in your professor's office, sweating bullets, as their academic associate is finalizing everyone's grades with a clicky-clacky keyboard.
+      So, are you ready to put your negotiation skills to the test and become the ultimate WAC negotiator? It's time to show your professor who's boss and negotiate your way to academic greatness!
     </p>
-    <p>
-      <a className="underline" href="https://github.com/ascorbic/daneel#readme">
-        Build your own
-      </a>
-    </p>
+
   </div>
 );
