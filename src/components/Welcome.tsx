46fc7adaa6bec03a0b68c135a50e5040f8f03a4d
arvindhsubramanian <90575131+arvindhsubramanian@users.noreply.github.com>
2023-04-14 01:33:45 +0530

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -2,13 +2,11 @@ export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
     <h1 className="text-2xl font-bold mb-2">👨‍🏫 Negotiation Games - WAC Grades! </h1>
     <p>
-      Are you tired of getting mediocre grades in WAC? 
+      Are you tired of getting mediocre grades in WAC? <br />
       
-      Are you convinced that your professor has a personal vendetta against you? 
+      Are you convinced that your professor has a personal vendetta against you? <br />
       
-      Picture this: you're sitting in your professor's office, sweating bullets, as their academic associate is finalizing everyone's grades with a clicky-clacky keyboard.
-      
-      So, are you ready to put your negotiation skills to the test? It's time to show your professor who's boss and negotiate your way to academic greatness!
+      It's time to show your professor who's boss and negotiate your way to academic greatness!
     </p>
 
   </div>
